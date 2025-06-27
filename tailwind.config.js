56103f9718507573b837d9751a8b4f1575b35d6e
GregLeDev <petrement88@gmail.com>
2025-06-27 23:34:09 +0200

    Installation de tailwinds CSS fonctionnel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts,scss",
-  ],
+  content: [],
   theme: {
     extend: {},
   },
-plugins: [],
+  plugins: [],
 }
+
